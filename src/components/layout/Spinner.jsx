72c65f47ea3ba86eb1fc11e3091c3dde4032f72f
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-01-01 12:55:17 +0000

    feat: added the loading spinner gif

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,11 @@
+import SpinnerIMG from './assets/spinner.gif'
+
+function Spinner() {
+    return (
+        <div className='w-100 mt-20'>
+            <img width={180} className='text-center mx-auto' src={SpinnerIMG} alt="Loading..." />
+        </div>
+    )
+}
+
+export default Spinner
