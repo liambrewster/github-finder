dfbd42e4965b39d5b35e1f7e1acdab853dc3f48c
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-01-05 20:54:32 +0000

    setting up the alert context and reducer ready for use

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import NotFound from './pages/NotFound'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import {GithubProvider} from './context/github/GithubContext'
+import {AlertProvider} from './context/alert/AlertContext'
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
       <Router >
         <div className="flex flex-col justify-between h-screen">
         <Navbar/>
@@ -23,6 +25,7 @@ function App() {
         <Footer/>
         </div>
       </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
