ad7ea63cd82e01c21b2f290530859bb17ec47bf9
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-01-08 00:27:18 +0000

    refactor: cleaned up the action file and added axios
    removed extra functions and condense into a single function

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,53 +1,31 @@
+import axios from 'axios'
+
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+
+const github = axios.create({
+    baseURL: GITHUB_URL,
+    headers: {Authorization: `token ${GITHUB_TOKEN}`}
+
+})
 
 //Get all users Search Results Function
 export const searchUsers = async (text) => {
     const params = new URLSearchParams({
         q: text
     })
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-        headers:{
-            Authorization: `token ${GITHUB_TOKEN}`
-        }
-    })
-    const {items} = await response.json()
+    const response = await github.get(`/search/user?${params}`)
 
-    return items
+    return response.data.items
     }
 
- //Get Single User data
- export const searchUser = async (login) => {
-    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
-        headers:{
-            Authorization: `token ${GITHUB_TOKEN}`
-        }
-    })
+//get the user and repo data at the same time
+export const getUserAndRepos = async (login) => {
+const [user, repos] = await Promise.all([
+    github.get(`/users/${login}`),
+    github.get(`/users/${login}/repos`)
+])
 
-    if(response.status === 404) {
-        window.location = '/notfound'
-    }else {
-        const data = await response.json()
-    
-        return data
-    }
-    }
+return {user: user.data, repos: repos.data}
 
-//get user repos data
-export const getUserRepos = async (login) => {
-
-    const params = new URLSearchParams({
-        sort: 'created',
-        per_page:10
-
-    })
-
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
-        headers:{
-            Authorization: `token ${GITHUB_TOKEN}`
-        }
-    })
-    const data = await response.json()
-
-    return data
-    }
+}
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -4,7 +4,7 @@ import GithubContext from "../context/github/GithubContext"
 import {FaCodepen, FaStore, FaUserFriends, FaUsers} from 'react-icons/fa'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
-import {searchUser,getUserRepos} from '../context/github/GithubActions'
+import {getUserAndRepos} from '../context/github/GithubActions'
 
 function User() {
 
@@ -14,11 +14,8 @@ function User() {
     useEffect(() => {
         dispatch({type:'SET_LOADING'})
         const getUserData = async() => {
-                const userData = await searchUser(params.login)
-                dispatch({type:'GET_USER', payload:userData})
-
-                const userRepoData = await getUserRepos(params.login)
-                dispatch({type:'GET_REPOS', payload:userRepoData})
+                const userData = await getUserAndRepos(params.login)
+                dispatch({type:'GET_USER_AND_REPOS', payload:userData})
 
         }
         getUserData()
