4d7a875809f7583a1c5d4ed08e51d869b8dacccc
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-01-08 00:00:22 +0000

    refactor: move the getUser function into the actions file

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,7 +1,7 @@
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
-//Get Search Results Function
+//Get all users Search Results Function
 export const searchUsers = async (text) => {
     const params = new URLSearchParams({
         q: text
@@ -14,4 +14,40 @@ export const searchUsers = async (text) => {
     const {items} = await response.json()
 
     return items
+    }
+
+ //Get Single User data
+ export const searchUser = async (login) => {
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+        headers:{
+            Authorization: `token ${GITHUB_TOKEN}`
+        }
+    })
+
+    if(response.status === 404) {
+        window.location = '/notfound'
+    }else {
+        const data = await response.json()
+    
+        return data
+    }
+    }
+
+//get user repos data
+export const getUserRepos = async (login) => {
+
+    const params = new URLSearchParams({
+        sort: 'created',
+        per_page:10
+
+    })
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+        headers:{
+            Authorization: `token ${GITHUB_TOKEN}`
+        }
+    })
+    const data = await response.json()
+
+    return data
     }
